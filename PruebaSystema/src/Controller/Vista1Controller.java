46685a24168167a10d8db70763e2cac00d8a13b1
notNamn <155586718+notNamn@users.noreply.github.com>
2024-08-05 16:44:23 -0500

    Add files via upload

---
+++ PruebaSystema/src/Controller/Vista1Controller.java
@@ -0,0 +1,26 @@
+
+package Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author HP
+ */
+public class Vista1Controller implements Initializable {
+    @FXML
+    private Label Label;
+    
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
